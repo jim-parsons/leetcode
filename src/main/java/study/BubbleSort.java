e9d716a86080445f41702ff9d138c98ad01de052
532066967@qq.com <zZ13270809297>
2019-03-04 15:45:53 +0800

    update bubble

--- src/main/java/study/BubbleSort.java
+++ src/main/java/study/BubbleSort.java
@@ -9,14 +9,14 @@ import static study.util.SortUtil.printArrs;
 public class BubbleSort {
 
     public static void sort(int[] arr) {
-        boolean flag = true;
         for (int i = 0; i < arr.length-1; i++) {
+        boolean flag = false;
             for (int j = 0; j < arr.length-i-1; j++) {
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
-                    flag = false;
+                    flag = true;
                 }
             }
             if (!flag) {
