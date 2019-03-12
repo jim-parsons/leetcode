6b938cb94647d7ec87f5805d3d2ecd6ad3a4275b
tang.chunbo <tang.chunbo@chinaredstar.com>
2019-03-12 20:49:43 +0800

    update LRU

--- src/main/java/hard/LRUCache.java
+++ src/main/java/hard/LRUCache.java
@@ -1,12 +1,19 @@
 package hard;
 
+import java.util.LinkedHashMap;
+
 /**
  * @date : 2019-02-14 15:13
  **/
-public class LRUCache {
+public class LRUCache<K, V> {
 
-    public static void main(String[] args) {
-        assert 1 < 0;
-        System.out.println("123");
+    private final LinkedHashMap<K, V> linkedHashMap;
+
+    public LRUCache(int maxSize) {
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("maxSize <= 0");
+        }
+
+        this.linkedHashMap = new LinkedHashMap<K, V>(16, 0.75f, true);
     }
 }
